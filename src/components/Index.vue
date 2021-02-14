6c500cd428e0032be0f7184248f2344361ec5446
rayfajars <rayfajars@gmail.com>
2021-02-14 19:23:09 +0700

    first commit

---
+++ src/components/Index.vue
@@ -0,0 +1,28 @@
+<template> 
+
+    <section>
+        <div class="container">
+        <div class="left"></div>
+        <div class="right">
+            <div class="content">
+                <h1>This is a heading</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure corrupti laboriosam excepturi suscipit, sit inventore quisquam blanditiis beatae fugit doloribus?</p>
+                <a href="#" class="btn">Click Me</a>
+            </div>
+        </div>
+        </div>
+    </section>
+    
+</template>
+
+<script>
+export default {
+    name: "Index"
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
